01391aada0302176da649ec1b5495a75567e3d27
Pius <mutundu.nguma@gmail.com>
2023-02-15 10:43:00 +0300

    making app skeleton

---
+++ src/components/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import '../App.css';
+import Header from './Header';
+
+function App() {
+ return (
+   <React.Fragment>
+     <div>
+       <Header />
+     </div>
+   </React.Fragment>   
+ )
+}
+
+export default App;
